2653c10941a33729c9bf90d5d2250c7317bd32d8
Jason Butz <jbutz@brake.com>
2012-11-29 14:07:31 -0600

    test

--- test/index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#jshint js/*.js --config js/.jshintrc
-#jshint js/tests/unit/*.js --config js/.jshintrc
-node test/server.js &
-phantomjs test/phantom.js "http://localhost:11000/test"
-#kill -9 `cat test/pid.txt`
-#rm test/pid.txt
